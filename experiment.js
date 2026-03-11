b6ab33d447551b383700176419b49260bf60c96b
jacksonp01 <jacksonpegg01@gmail.com>
2026-03-11 11:53:40 -0400

    fixup!

--- experiment.js
+++ experiment.js
@@ -2235,6 +2235,8 @@ function WordListTestRoutineBegin(snapshot) {
     row_vertical_positions = [(- 0.42), (- 0.32)];
     dynamic_buttons = [];
     dynamic_button_texts = [];
+    x_position;
+    y_position;
     label_index = 0;
     for (var row_index, _pj_c = 0, _pj_a = util.range(num_rows), _pj_b = _pj_a.length; (_pj_c < _pj_b); _pj_c += 1) {
         row_index = _pj_a[_pj_c];
